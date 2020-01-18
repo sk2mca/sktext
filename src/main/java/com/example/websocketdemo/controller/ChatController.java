7afd3acd6ccfb0e324210c7540faf21cce2261f2
Suresh P <sureshskca@gmail.com>
2020-01-18 23:14:28 +0530

    Initial commit

---
+++ src/main/java/com/example/websocketdemo/controller/ChatController.java
@@ -0,0 +1,47 @@
+package com.example.websocketdemo.controller;
+
+import com.example.websocketdemo.model.ChatMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
+import static java.lang.String.format;
+
+/**
+ * Created by rajeevkumarsingh on 24/07/17.
+ */
+@Controller
+public class ChatController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @MessageMapping("/chat/{roomId}/sendMessage")
+    public void sendMessage(@DestinationVariable String roomId, @Payload ChatMessage chatMessage) {
+        logger.info(roomId+" Chat messahe recieved is "+chatMessage.getContent());
+        messagingTemplate.convertAndSend(format("/chat-room/%s", roomId), chatMessage);
+    }
+
+    @MessageMapping("/chat/{roomId}/addUser")
+    public void addUser(@DestinationVariable String roomId, @Payload ChatMessage chatMessage,
+                        SimpMessageHeaderAccessor headerAccessor) {
+        String currentRoomId = (String) headerAccessor.getSessionAttributes().put("roomId", roomId);
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        if (currentRoomId != null) {
+            ChatMessage leaveMessage = new ChatMessage();
+            leaveMessage.setType(ChatMessage.MessageType.LEAVE);
+            leaveMessage.setSender(chatMessage.getSender());
+            messagingTemplate.convertAndSend(format("/chat-room/%s", currentRoomId), leaveMessage);
+        }
+        messagingTemplate.convertAndSend(format("/chat-room/%s", roomId), chatMessage);
+    }
+
+}
