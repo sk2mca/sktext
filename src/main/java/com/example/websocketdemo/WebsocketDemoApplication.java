7327a8561ffc03cd1f6193f2d2e3d036a0a72a7b
Suresh P <sureshskca@gmail.com>
2020-01-18 23:42:04 +0530

    changes by suresh

--- src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
+++ src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebsocketDemoApplication {
 
 	public static void main(String[] args) {
-        System.out.println("Application started in Spring boot");
+        System.out.println("Application started in Spring boot suresh");
 	    SpringApplication.run(WebsocketDemoApplication.class, args);
 	}
 }
