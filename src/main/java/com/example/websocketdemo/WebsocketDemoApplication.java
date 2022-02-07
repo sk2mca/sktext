0c1b5c3f21f6023355e362fee2443b784c7de92e
Suresh P <santhosh@relativity.co.in>
2022-02-07 08:48:55 +0530

    Docker conf removed

--- src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
+++ src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
@@ -2,9 +2,12 @@ package com.example.websocketdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class WebsocketDemoApplication {
+public class WebsocketDemoApplication  {
 
 	public static void main(String[] args) {
         System.out.println("Application started in Spring boot master");
